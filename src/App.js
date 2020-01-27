aeb31732ac8b1e034aa3cfcb8e0114cba6c40365
Adeyeye <x638132@am.abg.com>
2020-01-27 17:49:53 -0600

    Latest Commit 01/27/2020

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import { SearchBar } from './components/layout/SearchBar';
 import { Logs } from './components/logs/Logs';
@@ -18,17 +20,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
